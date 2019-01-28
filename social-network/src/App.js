67913fb14c015e33a488d9c8349c3f52bf97c84e
PivrikArtem <artempivrik90@gmail.com>
2019-01-28 20:14:07 +0200

    add new elements

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <header></header>
-      <nav></nav>
-      <section></section>
-      <footer></footer>
+      <header>
+        <img  src='https://www.talkpoint.com/wp-content/uploads/social-network-background-with-icons_23-2147497535.jpg'/>
+      </header>
+      <nav>Profile</nav>
+      <section>Main content</section>
+      <footer>Footer</footer>
 
     </div>
   );
