be70cf5e8376911076f0aed9ef5b2668fd4ee165
PivrikArtem <artempivrik90@gmail.com>
2019-02-02 22:55:57 +0200

    add new css

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -4,13 +4,44 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <header>
-        <img  src='https://www.talkpoint.com/wp-content/uploads/social-network-background-with-icons_23-2147497535.jpg'/>
+    <div className="app-wrapper">
+      <header className='header'>
+        <img src='https://image.flaticon.com/icons/svg/149/149990.svg' />
       </header>
-      <nav>Profile</nav>
-      <section>Main content</section>
-      <footer>Footer</footer>
+      <nav className='nav'>
+        <div>
+          <a href=''>Profile</a>
+        </div>
+        <div>
+          <a href=''>Messages</a>
+        </div>
+        <div>
+          <a href=''>News</a>
+        </div>
+        <div>
+          <a href=''>Music</a>
+        </div>
+        <div>
+          <a href=''>Setting</a>
+        </div>
+      </nav>
+      <section className='content'>
+        <div>
+        <img src='http://amandajevans.com/wp-content/uploads/2019/01/inspirational-motivational-quotes-6.jpg' />
+        </div>
+        <div>
+          ava+description
+       </div>
+        <div>
+          My posts
+       </div>
+        <div>
+          New posts
+         <div>post1</div>
+          <div>post2</div>
+        </div>
+      </section>
+      <footer className='footer'>Footer</footer>
 
     </div>
   );
