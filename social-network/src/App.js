432f374d54a2a2712b1f3660b349b8cdecb3bee8
PivrikArtem <artempivrik90@gmail.com>
2019-02-03 23:09:03 +0200

    splitting on components

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -1,48 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header className='header'>
-        <img src='https://image.flaticon.com/icons/svg/149/149990.svg' />
-      </header>
-      <nav className='nav'>
-        <div>
-          <a href=''>Profile</a>
-        </div>
-        <div>
-          <a href=''>Messages</a>
-        </div>
-        <div>
-          <a href=''>News</a>
-        </div>
-        <div>
-          <a href=''>Music</a>
-        </div>
-        <div>
-          <a href=''>Setting</a>
-        </div>
-      </nav>
-      <section className='content'>
-        <div>
-        <img src='http://amandajevans.com/wp-content/uploads/2019/01/inspirational-motivational-quotes-6.jpg' />
-        </div>
-        <div>
-          ava+description
-       </div>
-        <div>
-          My posts
-       </div>
-        <div>
-          New posts
-         <div>post1</div>
-          <div>post2</div>
-        </div>
-      </section>
-      <footer className='footer'>Footer</footer>
-
+      <Header />
+      <Navbar />
+      <Profile />
+      <Footer />
+      
     </div>
   );
 }
