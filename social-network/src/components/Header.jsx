b4891fd2b00ef89608c62ed2fbf961c6936a3a90
PivrikArtem <artempivrik90@gmail.com>
2019-02-03 23:10:56 +0200

    add new files

---
+++ social-network/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://image.flaticon.com/icons/svg/149/149990.svg' />
+        </header>
+    )
+}
+export default Header;
---
+++ social-network/src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <section className='content'>
+            <article>
+                <img src='http://amandajevans.com/wp-content/uploads/2019/01/inspirational-motivational-quotes-6.jpg' />
+            </article>
+            <article>
+                ava+description
+            </article>
+            <article>
+                My posts
+            </article>
+            <article>
+                New posts
+         <article>
+                    post1
+             </article>
+                <article>
+                    post2
+                    </article>
+            </article>
+        </section>
+    )
+}
+export default Profile;
